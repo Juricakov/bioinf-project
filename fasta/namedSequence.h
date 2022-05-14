9377efdb80a38ea34ad84b72b58fce456c39b209
Jurica Kovacevic <jurica.kovacevic99@gmail.com>
2022-05-14 15:29:42 +0200

    named sequence class definition update

--- fasta/namedSequence.h
+++ fasta/namedSequence.h
@@ -7,7 +7,7 @@ public:
     NamedSequnce(std::string name, std::string sequence);
     std::string name;
     std::string sequence;
-    // TODO Jurica implement
-    NamedSequnce complement();
+    // TODO Jurica
+    std::string complement();
     friend std::ostream &operator<<(std::ostream &os, const NamedSequnce &reading);
 };
